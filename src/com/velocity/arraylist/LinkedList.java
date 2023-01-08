6fba87abe515c344775b2b630267a764b39512fa
nayanband <nayanband97@gmail.com>
2023-01-08 15:34:42 +0530

    Added ArrayList Demo 2

---
+++ src/com/velocity/arraylist/LinkedList.java
@@ -0,0 +1,18 @@
+package com.velocity.arraylist;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LinkedList {
+
+	public static void main(String[] args) {
+		List<Integer> list=new ArrayList<Integer>();
+		list.add(10);
+		list.add(20);
+		list.add(30);
+		list.add(40);
+		System.out.println(list);
+		
+	}
+
+}
